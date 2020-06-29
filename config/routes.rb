b421cf589d71d8262c54c171885c9378b9e95e12
Pedro Fontes <pedrosafontes.psf@gmail.com>
2020-06-29 18:12:17 +0100

    removed pundit

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-resources :competitions do
-    resources :competitions, only: [:index]
+  resources :competitions, only: [:index]
 
   resources :competitions, only: [:show] do
     resources :registrations, only: [:new]
   end
+
   resources :federations, only: [:index, :show] do
     resources :affiliations, only: [:new]
 
