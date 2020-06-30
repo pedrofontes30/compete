4194a7e33e33f909e252cefa250ae4b5130d4a74
Pedro Fontes <pedrosafontes.psf@gmail.com>
2020-06-30 13:07:15 +0100

    Affiliate button only appears if user is not affiliated

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -2,15 +2,13 @@ class CompetitionsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-
     @competitions = policy_scope(Competition)
-
     @competitions = Competition.all
-
   end
 
   def show
     @competition = Competition.find(params[:id])
     authorize @competition
+    (@affiliated = (Affiliation.where(user: current_user, federation: @competition.federation) == [])) if current_user.present?
   end
 end
