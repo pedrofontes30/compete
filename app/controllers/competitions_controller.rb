e76ee99b52d3e6e597679db0ffd01786c02e3f1e
nunolei <nunomartinslei@gmail.com>
2020-06-30 12:00:56 +0100

    Federation Create

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -2,4 +2,9 @@ class CompetitionsController < ApplicationController
   def show
     @competition = Competition.find(params[:id])
   end
+  def new
+    @competition = Competition.new
+  end
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :competitions, only: [:show] do
+  resources :competitions, only: [:show, :new] do
     resources :registrations, only: [:new]
   end
   resources :federations, only: [:index, :show] do
