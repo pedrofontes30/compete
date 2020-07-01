c6c442ca1a8f5f00e2a7a0ed5f76bac1715f234e
Pedro Fontes <pedrosafontes.psf@gmail.com>
2020-07-01 16:58:05 +0100

    Listed registrations on user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,22 @@
 Rails.application.routes.draw do
 
-
-  #devise_for :federations
-  #devise_for :users
-
-
   devise_for :federations, path: 'federations', controllers: { sessions: "federations/sessions", registrations: "federations/registrations" }
   devise_for :users, path: 'users', controllers: { sessions: "users/sessions", registrations: "users/registrations" }
 
 
   root to: 'pages#home'
+
+  get '/profile', to: 'pages#profile'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   #Setup the page to redirect after sign up
   authenticated :user do
     root 'pages#home', as: :authenticated_user_root
   end
+
   authenticated :federation do
     root 'pages#home', as: :authenticated_federation_root
   end
-
 
   resources :competitions, only: [:show, :index] do
     resources :registrations, only: [:new, :create]
