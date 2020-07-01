38a083d7ba64bf057192c9e455bb07042785a735
Pedro Fontes <pedrosafontes.psf@gmail.com>
2020-07-01 17:54:18 +0100

    Solving param sanitizer issue

--- app/controllers/federations/parameter_sanitizer.rb
+++ app/controllers/federations/parameter_sanitizer.rb
@@ -1,4 +1,4 @@
-class FederationParameterSanitizer < Devise::ParameterSanitizer
+class Federations::ParameterSanitizer < Devise::ParameterSanitizer
   def initialize(*)
     super
     @params.permit(:sign_up, keys: [:name, :location, :affiliation_price])
