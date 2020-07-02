f15877182500ff9868d21b95e13c897bea0ff9c3
cmscoder <camila.medeiros.silva2013@gmail.com>
2020-07-02 14:16:05 +0100

    Upload Pictures is ok

--- app/controllers/users/parameter_sanitizer.rb
+++ app/controllers/users/parameter_sanitizer.rb
@@ -1,6 +1,6 @@
 class Users::ParameterSanitizer < Devise::ParameterSanitizer
   def initialize(*)
     super
-    @params.permit(:sign_up, keys: [:first_name, :last_name, :date_of_birth, :nationality, :gender])
+    @params.permit(:sign_up, keys: [:first_name, :last_name, :date_of_birth, :nationality, :gender, :photo])
   end
 end
--- app/models/competition.rb
+++ app/models/competition.rb
@@ -2,6 +2,7 @@ class Competition < ApplicationRecord
   belongs_to :federation
   has_many :competition_divisions
   has_many :registrations, through: :competition_divisions
+  has_one_attached :photo
 
   accepts_nested_attributes_for :competition_divisions, allow_destroy: true, reject_if: :all_blank
 end
